1d004c8bcd4fb96adf1c40ced60bd352dbdb0b7d
romanesss <romagavrilyuk2015@gmail.com>
2024-04-19 23:07:23 +0300

    cosmetic fix

--- tests/main.spec.ts
+++ tests/main.spec.ts
@@ -1,12 +1,12 @@
 import { test } from '../fixtures/baseFixture';
 import { expect } from '@playwright/test';
 
-const testData: { title: string; operation: 'Sum' | 'Divide' | 'Multiply' | 'Subtract'; numbersArray: [number, number]; result: string }[] = [
-    { title: 'Verify the calculator can accurately sum two numbers', operation: 'Sum', numbersArray: [2, 2], result: '4' },
-    { title: 'Ensure the calculator can accurately subtract one number from another', operation: 'Subtract', numbersArray: [2, 2], result: '0' },
-    { title: 'Multiply Test', operation: 'Multiply', numbersArray: [2, 2], result: '4' },
-    { title: 'Divide Test', operation: 'Divide', numbersArray: [2, 2], result: '1' },
-    { title: 'Division by Zero Test', operation: 'Divide', numbersArray: [2, 0], result: 'Not a Number' }
+const testData: { title: string; operation: 'Sum' | 'Divide' | 'Multiply' | 'Subtract'; numbers: [number, number]; result: string }[] = [
+    { title: 'Verify the calculator can accurately sum two numbers', operation: 'Sum', numbers: [2, 2], result: '4' },
+    { title: 'Ensure the calculator can accurately subtract one number from another', operation: 'Subtract', numbers: [2, 2], result: '0' },
+    { title: 'Multiply Test', operation: 'Multiply', numbers: [2, 2], result: '4' },
+    { title: 'Divide Test', operation: 'Divide', numbers: [2, 2], result: '1' },
+    { title: 'Division by Zero Test', operation: 'Divide', numbers: [2, 0], result: 'Not a Number' }
 ];
 
 test.beforeEach(async ({ calculatorPage }) => {
@@ -35,10 +35,9 @@ test('Ensure that using the full reset clears all previous calculations complete
     expect(await calculatorPage.getAnswerValue()).not.toContain('6');
 });
 
-for (const { title, operation, numbersArray, result } of testData) {
+for (const { title, operation, numbers, result } of testData) {
     test(`${title}`, async ({ calculatorPage }) => {
-        await calculatorPage.logicOperation({ numbers: numbersArray, operation: operation });
+        await calculatorPage.logicOperation({ numbers: numbers, operation: operation });
         expect(await calculatorPage.getAnswerValue()).toContain(result);
     });
 }
-
